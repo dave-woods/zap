ca77b34516dbba74fb96c3c888cd03d3bd1a8c56
Dave Woods <dwoodscs@gmail.com>
2017-04-11 02:49:06 +0100

    Add checkbox and slider

---
+++ src/js/components/BinaryChoice.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import '../../../assets/styles/BinaryChoice.css'
+
+export default class BinaryChoice extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      checked: this.props.checked || false
+    }
+  }
+
+  handleClick () {
+    this.setState(prevState => {
+      return {checked: !prevState.checked}
+    })
+  }
+
+  render () {
+    return (<div onClick={this.handleClick.bind(this)} className={this.props.type + (this.state.checked ? ' checked' : '')}>
+      <div />
+    </div>)
+  }
+}
+
+BinaryChoice.propTypes = {
+  type: PropTypes.oneOf(['checkbox', 'slider']).isRequired,
+  checked: PropTypes.bool
+}
