c4df0a636a332f607e453c390d5355b0a3e0c3d6
Dave Woods <dwoodscs@gmail.com>
2017-04-13 16:25:20 +0100

    Updated Form functionality

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 import BinaryChoice from './BinaryChoice.js'
 import Button from './Button.js'
+import {Dropdown, Option} from './Dropdown.js'
 
 import '../../css/Form.css'
 
@@ -10,22 +11,44 @@ export default class Form extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      formObj: false
+      formObj: false,
+      textValue: '',
+      dropdownValue: ''
     }
+
+    this.handleText = this.handleText.bind(this)
+    this.handleDropdown = this.handleDropdown.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleSubmit () {
+  handleText (e) {
+    this.setState({textValue: e.target.value})
+  }
+
+  handleDropdown (e) {
+    this.setState({dropdownValue: e.target.value})
+  }
+
+  handleSubmit (e) {
+    e.preventDefault()
+    const sText = document.getElementById('submit-text')
+    sText.innerHTML = 'Submitted ' + (this.state.textValue || 'nothing') + ' and ' + (this.state.dropdownValue || 'nothing')
     this.setState({formObj: true})
+    setTimeout(() => { sText.innerHTML = '' }, 2000)
   }
 
   render () {
     return (
-      <div className='form'>
-        <input type='text' /><BinaryChoice type='checkbox' />
+      <form onSubmit={this.handleSubmit} className='form'>
+        <input onChange={this.handleText} value={this.state.textValue} type='text' />
+        <BinaryChoice type='checkbox' />
+        <Dropdown handleChange={this.handleDropdown}>
+          <Option value='one' />
+          <Option value='two' text='Two' />
+        </Dropdown>
         <Button onClick={this.handleSubmit}>Submit</Button>
-        { this.state.formObj && <span>Submitted!</span> }
-      </div>
+        <span id='submit-text' />
+      </form>
     )
   }
 }
